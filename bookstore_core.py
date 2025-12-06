4aaf42d1e791134a9e137e10269d6f4551f3f630
Autumn <100223878+YourUnravelling@users.noreply.github.com>
2025-12-06 18:30:09 +0000

    some basic functions

--- bookstore_core.py
+++ bookstore_core.py
@@ -5,6 +5,37 @@ Autumn Hickinbotham - 11/25
 
 import os
 
-DEFAULT_FILENAME = "bookstore_gui.pyw"
+DEFAULT_PATH:str = "bookstore"
 
-filename:str = DEFAULT_FILENAME
+base_path:str = DEFAULT_PATH
+
+def rangecalc(value:int|float,max:int|float|None=None,min:int|float|None=None) -> bool:
+    if min:
+        if value < min:
+            return False
+    if max:
+        if value > max:
+            return False
+    return True
+
+def validate_str(inp:str,maxlen:int = -1, minlen:int = -1):
+    """Returns """
+
+    if not rangecalc(len(inp),max=maxlen,min=minlen):
+        return False
+    
+    return True
+
+def validate_int(inp:str|int, max:int|None = None, min:int|None = None):
+    """Returns true if the inputted integer is valid according to the parameters
+    `inp` = 
+    `max`, `min` = Inclusive
+    """
+    try:
+        int_inp = int(inp)
+    except:
+        return False
+    if not rangecalc(int_inp,max=max,min=min):
+        return False
+    
+    return True
--- bookstore_gui.pyw
+++ bookstore_gui.pyw
@@ -5,3 +5,12 @@ Autumn Hickinbotham - 11/25
 
 import bookstore_core as core
 import tkinter as tk
+
+w = tk.Tk()
+
+def main():
+    w.mainloop()
+
+if __name__ == "__main__":
+    main()
+
