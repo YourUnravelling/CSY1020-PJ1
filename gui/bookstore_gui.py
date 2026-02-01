505195c85bd66dc08cb798025bb2b3a2251b8865
Autumn <100223878+YourUnravelling@users.noreply.github.com>
2026-02-01 16:37:29 +0000

    Deleted unused class in gui

--- gui/bookstore_gui.py
+++ gui/bookstore_gui.py
@@ -78,54 +78,6 @@ class MainLayout(DFrame):
     def core(self):
         return self.__core
 
-class DBViewer(DFrame): # TODO Unused
-    def __init__(self, parent, core):
-        super().__init__(parent)
-        self.__parent = parent # Private, TODO maybe delete
-        self.__sm = core.sm # Private, Pointer to an SQLManager instance which executes sql
-        self.__core = core
-        self.__config = core.config
-
-        self.__topbar = DFrame(self)
-        self.__topbar.pack(fill="x", padx=5, pady=5)
-        self.__content = DFrame(self)
-        self.__content.pack(expand=True, fill="both")
-
-        tables_list = list(table for table in self.__sm.schema)
-        self.__table_selector = DoubleCombobox(self.__topbar, 
-                state="normal", 
-                on_select= self.__table_selected,
-                raw= tables_list, 
-                display= list(table.capitalize() for table in tables_list), 
-                default= tables_list.index(self.__core.config.default_table),
-                creation_call = True)
-
-        self.__table_selector.pack(side=tk.LEFT)
-
-        self.__view_edit_button = tk.Button(self.__topbar, text="Viewing")
-        self.__view_edit_button.pack(side="right")
-
-        self.pack(fill="both", expand=True)
-
-        self.__viewer = RecordViewer(self, self.__content, "book")
-        self.__viewer.pack(fill="both",expand=True)
-
-
-    def set_table(self, table):
-        """Sets the table"""
-        self.__table_selected(table)
-
-    def __table_selected(self, index, tablename):
-        self.__viewer.set_table(tablename)
-
-    @property
-    def sm(self):
-        return self.__sm
-    
-    @property
-    def core(self):
-        return self.__core
-
 
 w = tk.Tk()
 w.iconphoto(True,tk.PhotoImage(file="resources/icon.png"))
